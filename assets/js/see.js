b2a0a5e2aced1e1096b2195d82f26738d6fbbd6c
minsoehan <minsoehan243@gmail.com>
2025-10-22 15:11:09 +0630

    updated

--- assets/js/see.js
+++ assets/js/see.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     copyLink();
+    titleToggle();
 });
 
 // copy the link to clipboard
@@ -12,6 +13,23 @@ function copyLink() {
             const link = `${window.location.origin}${window.location.pathname}#${titleId}`;
             navigator.clipboard.writeText(link);
             alert('The link is copied.');
+        });
+    });
+}
+
+function titleToggle() {
+    const titleBtns = document.querySelectorAll('.see-container > h3');
+    titleBtns.forEach(titleBtn => {
+        titleBtn.addEventListener('click', () => {
+            let sibling = titleBtn.nextElementSibling;
+            while (sibling) {
+                if (sibling.style.display === 'block' || sibling.style.display === '') {
+                    sibling.style.display = 'none';
+                } else {
+                    sibling.style.display = 'block';
+                }
+                sibling = sibling.nextElementSibling;
+            }
         });
     });
 }
