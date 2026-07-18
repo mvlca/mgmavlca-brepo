ab2375aac386c44aaa9b4448d9b7ea03b0667877
minsoehan <minsoehan243@gmail.com>
2026-07-18 20:29:21 +0630

    updated

--- assets/js/reports.js
+++ assets/js/reports.js
@@ -1,4 +1,36 @@
-document.addEventListener('DOMContentLoaded', async () => {
+let reports = [];
+function updateCount() {
+    const count = document.getElementById('reports-count');
+    const visible = Array.from(reports).filter(el => el.style.display !== 'none').length;
+    count.textContent = visible.toString();
+}
+const input = document.querySelector('form.reports-search input');
+function searchReports() {
+    input.addEventListener('input', () => {
+        const value = input.value.toLowerCase();
+        reports.forEach((report) => {
+            const text = report.textContent.toLowerCase() || "";
+            report.style.display = text.includes(value) ? '' : 'none';
+        });
+        updateCount();
+    });
+}
+const form = document.querySelector('form.reports-search');
+function clearSearch() {
+    form.addEventListener('reset', () => {
+        input.value = '';
+        reports.forEach((report) => {
+            report.style.display = '';
+        });
+        updateCount();
+    });
+}
+function preventFromSubmit() {
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+    });
+}
+async function reportsList() {
     const ctn = document.getElementById('all-reports');
     const temp = document.getElementById('reports-template');
     if (!ctn || !temp) return;
@@ -12,17 +44,19 @@ document.addEventListener('DOMContentLoaded', async () => {
         const text = res ? (await res.text()) : "No data found";
         ctn.textContent = "";
         const lines = text.split(/\r?\n/);
-        lines.slice(1).map(line => {
+        reports = lines.slice(1).map(line => {
             const cols = line.split('\t');
             const clone = temp.content.cloneNode(true);
-            if (!clone) return;
-            const name = clone.querySelector('.each-report .name');
-            const grade = clone.querySelector('.each-report .grade');
-            const status = clone.querySelector('.each-report .status-line .status');
-            const country = clone.querySelector('.each-report .status-line .country');
-            const period = clone.querySelector('.each-report .status-line .period');
-            const date = clone.querySelector('.each-report .status-line .date');
-            const timestamp = clone.querySelector('.each-report .status-line .timestamp');
+            const each = clone.querySelector('div.each-report');
+            if (!clone || !each) return;
+            const name = each.querySelector('.name');
+            const grade = each.querySelector('.grade');
+            const country = each.querySelector('div .country');
+            const workers = each.querySelector('div .workers');
+            const status = each.querySelector('.status-line .status');
+            const period = each.querySelector('.status-line .period');
+            const date = each.querySelector('.status-line .date');
+            const timestamp = each.querySelector('.status-line .timestamp');
             const a = document.createElement('a');
             a.href = `https://reports.mgmavlca.org/members/report?rid=${cols[1]}`;
             a.textContent = cols[7];
@@ -30,13 +64,23 @@ document.addEventListener('DOMContentLoaded', async () => {
             if (grade) grade.textContent = cols[6];
             if (status) status.textContent = cols[11];
             if (country) country.textContent = cols[8];
+            if (workers) workers.textContent = cols[10];
             if (period) period.textContent = cols[3];
             if (date) date.textContent = cols[2];
             if (timestamp) timestamp.textContent = cols[0];
             ctn.appendChild(clone);
+            return each;
         });
+        updateCount();
     } catch (err) {
         const error = err instanceof Error ? err.message : String(err);
         ctn.textContent = `Caught: ${error}`;
     }
+}
+document.addEventListener('DOMContentLoaded', () => {
+    reportsList();
+    updateCount();
+    searchReports();
+    clearSearch();
+    preventFromSubmit();
 });
--- content/reports/index.md
+++ content/reports/index.md
@@ -1,9 +1,12 @@
 +++
 date = '2026-07-17T12:42:26+06:30'
 draft = false
-title = 'Reports'
+title = 'VLCA Reports'
+layout = 'plainpage'
 [params.page]
     add = 'reports'
 +++
+
+### VLCA Reports 2026
 
 {{< include-partial "records/reports-template" >}}
